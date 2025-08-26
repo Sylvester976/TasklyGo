ed45aa3f4371cc3ec8d8ebe1db796c1321b3a6e5
Sylvester Nyabenge <officialotienosylvester@gmail.com>
2025-08-26 18:08:14 +0300

    Update db.go

--- web/db/db.go
+++ web/db/db.go
@@ -12,13 +12,20 @@ import (
 var Pool *pgxpool.Pool
 
 func ConnectDB() {
-	// Load .env
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	// Load .env only if it exists (for local development)
+	if _, err := os.Stat(".env"); err == nil {
+		if err := godotenv.Load(); err != nil {
+			log.Println("Warning: .env file found but could not be loaded:", err)
+		} else {
+			log.Println(".env file loaded")
+		}
 	}
 
+	// Get DATABASE_URL from environment variables
 	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL not set. Make sure you have a .env file locally or set the env variable on Render")
+	}
 
 	// Connect to PostgreSQL
 	pool, err := pgxpool.New(context.Background(), dbURL)
