64cef45d050726fa8c0c07120f87565fa1c384e0
Sylvester976 <officialotienosylvester@gmail.com>
2025-08-29 16:10:58 +0300

    we start off with the task ui v1

--- web2/routes/routes.go
+++ web2/routes/routes.go
@@ -10,4 +10,7 @@ func SetupRoutes() {
 	http.HandleFunc("/register", handlers.RegisterHandler)
 	http.HandleFunc("/signup", handlers.AuthRegisterHandler)
 	http.HandleFunc("/login", handlers.AuthLoginHandler)
+	http.HandleFunc("/task", handlers.staffTaskHandler)
+	http.HandleFunc("/manager", handlers.managerTaskHandler)
+
 }
